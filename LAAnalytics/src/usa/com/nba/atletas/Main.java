55c4469edf1ca71f5482584500303255c1a295ee
lab04 <lab04@lab4-03>
2024-09-16 20:58:03 -0300

    *Adicionado Menu Bar

--- LAAnalytics/src/usa/com/nba/atletas/Main.java
+++ LAAnalytics/src/usa/com/nba/atletas/Main.java
@@ -1,26 +1,32 @@
 package usa.com.nba.atletas;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
-
+import javafx.scene.control.SplitPane;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage palcoPrincipal) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("telaUm.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			Scene cenario = new Scene(root);
+			cenario.getStylesheets().add(getClass().getResource("estilo.css").toExternalForm());
+			palcoPrincipal.setScene(cenario);
+			
+			palcoPrincipal.setTitle("LAANALYTICS BRAZIL LCC");
+			
+			palcoPrincipal.getIcons().add(new Image(getClass().getResourceAsStream("/usa/com/nba/atletas/bolaicone.png")));
+			
+			palcoPrincipal.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
